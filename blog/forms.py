d4f4de932ed83141129ea0bd410d3cdfbd68552e
Landon Koo <landonkoo0207@gmail.com>
2017-08-30 17:30:23 +1000

    test

--- blog/forms.py
+++ blog/forms.py
@@ -1,25 +1,25 @@
-from django.contrib.auth.forms import AuthenticationForm
-from django import forms
-from .models import Post, Category
-from datetime import datetime
-from froala_editor.widgets import FroalaEditor
+# from django.contrib.auth.forms import AuthenticationForm
+# from django import forms
+# from .models import Post, Category
+# from datetime import datetime
+# from froala_editor.widgets import FroalaEditor
 
-class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
-    password = forms.CharField(label="Password", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+# class LoginForm(AuthenticationForm):
+#     username = forms.CharField(label="Username", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+#     password = forms.CharField(label="Password", max_length=30, widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
 
-class PostForm(forms.ModelForm):
-    title = forms.CharField(max_length=50, required=True)
-    category = forms.ChoiceField(choices=[x for x in Category.objects.all()], required=True)
-    contents = forms.CharField(widget=FroalaEditor, required=True)
-    time_updated = forms.DateTimeField(widget=forms.HiddenInput())
+# class PostForm(forms.ModelForm):
+#     title = forms.CharField(max_length=50, required=True)
+#     category = forms.ChoiceField(choices=[x for x in Category.objects.all()], required=True)
+#     contents = forms.CharField(widget=FroalaEditor, required=True)
+#     time_updated = forms.DateTimeField(widget=forms.HiddenInput())
 
-    class Meta:
-        model = Post
-        fields = ['title', 'category', 'contents', 'time_updated']
+#     class Meta:
+#         model = Post
+#         fields = ['title', 'category', 'contents', 'time_updated']
 
-    def __init__(self, *args, **kwargs):
-        super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['category'].choices = [('', 'Please choose the category.'),] + list(self.fields['category'].choices)[1:]
+#     def __init__(self, *args, **kwargs):
+#         super(PostForm, self).__init__(*args, **kwargs)
+#         self.fields['category'].choices = [('', 'Please choose the category.'),] + list(self.fields['category'].choices)[1:]
 
 
