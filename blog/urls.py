e41c68a8cf9afa994421f60ea3344644ef6a348c
Landon Koo <landonkoo0207@gmail.com>
2017-09-05 18:33:59 +1000

    Home menu in the main navbar removed

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    #url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^blog/$', views.PostListView.as_view(), name='post-list'),
+    url(r'^$', views.PostListView.as_view(), name='post-list'),
     url(r'^create/$', views.PostCreateView.as_view(success_url="/blog"), name='post_create'),
     url(r'^update/(?P<pk>\d+)/$', views.PostUpdateView.as_view(), name='post_update'),
     url(r'^delete/(?P<id>\d+)/$', views.PostDeleteView, name="post_delete"),
