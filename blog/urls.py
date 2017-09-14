9c40d4070a914c66d5d34a159ab25a3f4c0c9f58
Landon Koo <landonkoo0207@gmail.com>
2017-09-14 14:57:44 +1000

    Contact page added

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     #url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^$', views.PostListView.as_view(), name='post-list'),
-    url(r'^contact/$', views.contact, name='contact')
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^post/(?P<slug>[-\w]+)/$', views.PostDetailView.as_view(), name='post_detail'),
     url(r'^create/$', views.PostCreateView.as_view(success_url="/blog"), name='post_create'),
     url(r'^update/(?P<pk>\d+)/$', views.PostUpdateView.as_view(), name='post_update'),
