f9f25cf1944972fcb38cac3ef967f36fedfd0108
Landon Koo <landonkoo0207@gmail.com>
2017-10-02 19:24:57 +1100

    tinymce settings changed

--- lktheprogrammer/settings.py
+++ lktheprogrammer/settings.py
@@ -139,7 +139,8 @@ TINYMCE_DEFAULT_CONFIG = {
     'selector': 'textarea',
     'theme': 'modern',
     'plugins': 'link image preview codesample contextmenu table code lists',
-    'toolbar1': 'bold italic underline | alignleft aligncenter alignright alignjustify ',
+    'toolbar1': 'bold italic underline | alignleft aligncenter alignright alignjustify '
+               '| bullist numlist | outdent indent | table | link image | codesample | preview code',
     'contextmenu': 'formats | link image',
     'menubar': False,
     'inline': False,
