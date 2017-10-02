443a1305914e332212a76cad53f72bbbb31189f8
Landon Koo <landonkoo0207@gmail.com>
2017-10-02 19:21:29 +1100

    tinymce settings changed

--- lktheprogrammer/settings.py
+++ lktheprogrammer/settings.py
@@ -139,8 +139,7 @@ TINYMCE_DEFAULT_CONFIG DEFAULT = {
     'selector': 'textarea',
     'theme': 'modern',
     'plugins': 'link image preview codesample contextmenu table code lists',
-    'toolbar1': 'bold italic underline | alignleft aligncenter alignright alignjustify '
-               '| bullist numlist | outdent indent | table | link image | codesample | preview code',
+    'toolbar1': 'bold italic underline | alignleft aligncenter alignright alignjustify ',
     'contextmenu': 'formats | link image',
     'menubar': False,
     'inline': False,
