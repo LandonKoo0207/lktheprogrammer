b44c089ce77d7b54fe8cb822227f870cff71cb8e
Landon Koo <landonkoo0207@gmail.com>
2017-10-02 16:36:11 +1100

    tinymce settings.py settings changed

--- lktheprogrammer/settings.py
+++ lktheprogrammer/settings.py
@@ -127,16 +127,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# STATICFILES_DIRS = [
-#      os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+     os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_REDIRECT_URL = '/'
 
 TINYMCE_DEFAULT_CONFIG = {
-    'plugins': "table,spellchecker,paste,searchreplace",
+    'plugins': "table,paste,searchreplace",
     'theme': "advanced",
     'cleanup_on_startup': True,
     'custom_undo_redo_levels': 10,
